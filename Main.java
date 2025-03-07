8ed1634a6f6ddbbedfc82fd64c584ff6d152f63a
Nguyen Anh Tuan <22521604@gm.uit.edu.vn>
2025-03-07 15:00:09 +0700

    Nop bai

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+/*### Bài 1 (4 điểm):
+
+Chỉ sử dụng bán kính $r$, không được sử dụng bất kỳ hằng số nào khác, hãy xấp xỉ diện tích của hình tròn tâm $O(0 ,0)$ bán kính $r$.*/
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
